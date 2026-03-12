fd2113290963d95c9d361c6f9dfd0ca75b0ac0f4
FaseehFS <faseeh1080@gmail.com>
2026-03-12 15:30:44 +0530

    Update images and card

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,7 +10,7 @@ type CardProps = {
 
 export default function Card({ image, title, text, children }: CardProps) {
   return (
-    <div className="bg-white rounded text-center p-4 text-black border-gray-300 overflow-hidden">
+    <div className="flex flex-col gap-4 p-4 bg-white rounded text-center text-black border-gray-300 overflow-hidden">
       {image && (
         <Image
           src={image}
@@ -20,9 +20,10 @@ export default function Card({ image, title, text, children }: CardProps) {
           className="w-full h-auto"
         />
       )}
-      {title && <h1 className="text-xl">{title}</h1>}
-      {text && <p>{text}</p>}
-
+      <div>
+        {title && <h1 className="text-xl">{title}</h1>}
+        {text && <p>{text}</p>}
+      </div>
       {children}
     </div>
   );
