1bc7d3ccacd1a781be19a58a6e8b0ab5d3b8a77b
FaseehFS <faseeh1080@gmail.com>
2026-03-14 16:24:16 +0530

    Add WIP pages using WIP component

---
+++ components/WorkInProgress.tsx
@@ -0,0 +1,10 @@
+export default function WorkInProgress() {
+  return (
+    <div className="w-full p-4 mt-8">
+      <h1 className="text-4xl text-center mb-4">Work in progress 🏗️</h1>
+      <p className="text-center">
+        This page is currently work in progress. Please return later.
+      </p>
+    </div>
+  );
+}
