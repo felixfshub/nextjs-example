536d068a8225c29ef679cd0d2509a036ce0768fe
FaseehFS <faseeh1080@gmail.com>
2026-03-17 09:11:37 +0530

    Use database

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,21 @@
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
 export async function GET() {
-  return Response.json({ message: "Hello from api!" });
+  try {
+    await sql`SELECT 1;`;
+
+    return Response.json(
+      { message: "Hello from api, db is working" },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error(err);
+
+    return Response.json(
+      { message: "Hello from api, db is not working" },
+      { status: 500 },
+    );
+  }
 }
