242375c33822f2ed9f6640852f00e447df9df3c0
FaseehFS <faseeh1080@gmail.com>
2026-03-19 21:33:50 +0530

    Use (with-nav)

---
+++ app/(with-nav)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+
+export default function WithNavLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import ThemeController from "@/components/ThemeController";
 
 const geistSans = Geist({
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`bg-bg text-text ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeController />
-        <Navbar />
         {children}
       </body>
     </html>
