bfb85182c8c7cc424299c5735612c9b2bfda4afe
FaseehFS <faseeh1080@gmail.com>
2026-03-20 22:07:58 +0530

    Update landing page layout for improved visual hierarchy

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center w-full max-w-lg mx-auto p-4">
+    <div className="flex flex-col w-full min-h-[75vh] justify-center items-center max-w-lg mx-auto p-4">
       <h1 className="text-4xl my-4">Welcome</h1>
       <Link href="/sign-in/">
         <Button>Sign in</Button>
