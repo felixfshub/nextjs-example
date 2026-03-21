02a05773b8e43d91f1e6f983a172faace9dda6d7
FaseehFS <faseeh1080@gmail.com>
2026-03-21 21:49:35 +0530

    Set supabase adapter

--- auth.ts
+++ auth.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
 import GitHub from "next-auth/providers/github";
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Resend, GitHub],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
 });
