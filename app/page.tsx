16421288bb131891b7f171c3772a7a794720af05
FaseehFS <faseeh1080@gmail.com>
2026-03-24 20:04:59 +0530

    style: add border to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import FSImage from "@/assets/fs-nobg-cropped.png";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col gap-4 w-full min-h-[75vh] justify-center items-center max-w-lg mx-auto p-4">
-      <Image src={FSImage} className="w-20" alt="fs Image" priority />
-      <h1 className="text-6xl">welcome!</h1>
-      <Link href="/home">
-        <Button size="lg">Sign in</Button>
-      </Link>
+    <div className="flex  w-full min-h-[75vh] justify-center items-center">
+      <div className="flex flex-col border border-border rounded-lg gap-4 justify-center items-center max-w-lg mx-auto p-8">
+        <Image src={FSImage} className="w-30" alt="fs Image" priority />
+        <h1 className="text-6xl">welcome!</h1>
+        <Link href="/home">
+          <Button size="lg">Sign in</Button>
+        </Link>
+      </div>
     </div>
   );
 }
