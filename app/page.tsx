106b5a499a65333fc5cd343120ffc4044a4a6c73
FaseehFS <faseeh1080@gmail.com>
2026-03-24 20:27:01 +0530

    feat: add and use cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,37 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import FSImage from "@/assets/fs-nobg-cropped.png";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function LandingPage() {
   return (
     <div className="flex  w-full min-h-[75vh] justify-center items-center">
-      <div className="flex flex-col border border-border rounded-lg gap-4 justify-center items-center max-w-lg mx-auto p-8">
-        <Image src={FSImage} className="w-30" alt="fs Image" priority />
-        <h1 className="text-6xl">welcome!</h1>
-        <Link href="/home">
-          <Button size="lg">Sign in</Button>
-        </Link>
-      </div>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-center mb-2">
+            Welcome to Next.js Example!
+          </CardTitle>
+          <CardDescription className="text-center">
+            Hey, thanks for visiting. This is my Next.js example application
+            which I made to learn Next.js.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Link href="/home" className="w-full">
+            <Button size="lg" className="w-full">
+              Open App
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,50 +5,71 @@ import GitHubIcon from "@/components/icons/github-icon";
 import Input from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function SignIn() {
   return (
     <div className="flex w-full min-h-[75vh] justify-center items-center max-w-lg mx-auto p-4">
-      <div className="flex flex-col w-full gap-2 border border-border p-4 rounded-lg">
-        <h1 className="text-center text-2xl mb-4">Sign in with Email</h1>
-        <form className="flex flex-col sm:flex-row w-full gap-4">
-          <Input
-            type="email"
-            name="email"
-            className="flex-1"
-            placeholder="Currently not supported"
-            disabled
-          />
-          <Button
-            type="submit"
-            size="lg"
-            className="shrink-0 whitespace-nowrap"
-            disabled
-          >
-            Sign In
-          </Button>
-        </form>
-        <div className="flex items-center gap-2">
-          <hr className="flex-1 text-border" />
-          <p>or</p>
-          <hr className="flex-1 text-border" />
-        </div>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+          <CardDescription>
+            Before you can open the app, you have to sign in.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-4">
+            <form className="flex flex-col sm:flex-row w-full gap-4">
+              <Input
+                type="email"
+                name="email"
+                className="flex-1"
+                placeholder="Currently not supported"
+                disabled
+              />
+              <Button
+                type="submit"
+                size="lg"
+                className="shrink-0 whitespace-nowrap"
+                disabled
+              >
+                Sign In
+              </Button>
+            </form>
 
-        <Button
-          variant="secondary"
-          onClick={() => signIn("google", { redirectTo: "/home" })}
-        >
-          <GoogleIcon className="w-4 h-4" />
-          Sign in with Google
-        </Button>
+            <div className="flex items-center gap-2">
+              <hr className="flex-1 text-border" />
+              <p>or</p>
+              <hr className="flex-1 text-border" />
+            </div>
 
-        <Button
-          variant="secondary"
-          onClick={() => signIn("github", { redirectTo: "/home" })}
-        >
-          <GitHubIcon className="w-4 h-4" /> Sign in with GitHub
-        </Button>
-      </div>
+            <div className="flex flex-col gap-2">
+              <Button
+                variant="secondary"
+                onClick={() => signIn("google", { redirectTo: "/home" })}
+              >
+                <GoogleIcon className="w-4 h-4" />
+                Sign in with Google
+              </Button>
+
+              <Button
+                variant="secondary"
+                onClick={() => signIn("github", { redirectTo: "/home" })}
+              >
+                <GitHubIcon className="w-4 h-4" /> Sign in with GitHub
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
