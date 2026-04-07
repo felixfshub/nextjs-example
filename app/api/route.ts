13df31785935cd07b7acdbde545ea202eae7a68e
FaseehFS <faseeh1080@gmail.com>
2026-04-07 20:18:47 +0530

    remove postgres

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,19 +1,6 @@
-import sql from "@/lib/db";
-
 export async function GET() {
-  try {
-    await sql`SELECT 1;`;
-
-    return Response.json(
-      { message: "Hello from api, db is working" },
-      { status: 200 },
-    );
-  } catch (err) {
-    console.error(err);
-
-    return Response.json(
-      { message: "Hello from api, db is not working" },
-      { status: 500 },
-    );
-  }
+  return Response.json(
+    { message: "Hello from api, db is under development" },
+    { status: 500 },
+  );
 }
