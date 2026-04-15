f586bf9a1872f3b5006e50852f54f26ad537cac9
FaseehFS <faseeh1080@gmail.com>
2026-04-15 17:17:51 +0530

    add description to react page

--- app/(user)/feature/react/page.tsx
+++ app/(user)/feature/react/page.tsx
@@ -5,6 +5,12 @@ export default function ReactPage() {
     <ArticleContainer>
       <Article>
         <h1>React</h1>
+        <p>
+          React lets you build user interfaces out of individual pieces called
+          components. Create your own React components like Thumbnail,
+          LikeButton, and Video. Then combine them into entire screens, pages,
+          and apps.
+        </p>
       </Article>
     </ArticleContainer>
   );
